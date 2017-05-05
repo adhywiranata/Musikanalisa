b440621548260532c1b5098125e9674336ac2ff5
Adhy <adhywiranatap@gmail.com>
2017-05-05 12:52:52 +0700

    added login button and redirects

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
+import Login from './containers/Login';
+import AuthSuccess from './containers/AuthSuccess';
 import MyArtists from './containers/MyArtists';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>MusiKanalisa</h2>
+      <BrowserRouter>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>MusiKanalisa</h2>
+          </div>
+          <Route exact path="/" component={MyArtists} />
+          <Route path="/login" component={Login} />
+          <Route path="/callback/:access_token/:refresh_token" component={AuthSuccess} />
         </div>
-        <MyArtists />
-      </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/containers/Login.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import axios from 'axios';
+
+const styles = {
+  list: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    padding: 40,
+    boxSizing: 'border-box',
+  },
+  btn: {
+    backgroundColor: '#1ED760',
+    padding: 20,
+    border: 0,
+    borderRadius: 10,
+    fontSize: '1.3em',
+    cursor: 'pointer',
+    color: '#FFFFFF',
+    outline: 'none',
+  },
+}
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <div style={styles.container}>
+        <h2>Login to Musikanalisa</h2>
+          <a href="http://localhost:1234/login/">
+            <button style={styles.btn}>
+              Login via Spotify
+            </button>
+          </a>
+      </div>
+    );
+  }
+}
+
+export default Login;
