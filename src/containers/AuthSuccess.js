373467dd15aa192f454ca1af3d2b53f6f106f9de
Adhy <adhywiranatap@gmail.com>
2017-05-05 12:53:19 +0700

    added auth success page and set tokens to local storage

---
+++ src/containers/AuthSuccess.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+const styles = {
+  list: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    padding: 40,
+    boxSizing: 'border-box',
+  },
+  btn: {
+    backgroundColor: '#1ED760',
+    padding: 20,
+    border: 0,
+    borderRadius: 10,
+    fontSize: '1.3em',
+    cursor: 'pointer',
+    color: '#FFFFFF',
+    outline: 'none',
+  },
+}
+
+class AuthSuccess extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  componentDidMount() {
+    const { access_token, refresh_token } = this.props.match.params;
+    localStorage.setItem('accessToken', access_token);
+    localStorage.setItem('refreshToken', refresh_token);
+  }
+
+  render() {
+    return (
+      <div style={styles.container}>
+        <h2>Auth Success!</h2>
+        <Link to="/">
+          <button style={styles.btn}>
+            Continue
+          </button>
+        </Link>
+      </div>
+    );
+  }
+}
+
+export default AuthSuccess;
