1b526597b8d8bfacc640d9b0df4a0cdc2bc9ed47
Adhy <adhywiranatap@gmail.com>
2017-05-09 13:29:22 +0700

    added link button hovers

--- src/components/LinkButton.js
+++ src/components/LinkButton.js
@@ -3,14 +3,21 @@ import { Link } from 'react-router-dom';
 import glamorous from 'glamorous';
 
 const Button = glamorous.button({
-  backgroundColor: '#1ED760',
+  backgroundColor: 'transparent',
   padding: 20,
-  border: 0,
+  border: '1px solid #1ED760',
   borderRadius: 10,
   fontSize: '1.2em',
   cursor: 'pointer',
-  color: '#FFFFFF',
+  color: '#1ED760',
   outline: 'none',
+  transition: 0.5,
+  ':hover': {
+    backgroundColor: '#1ED760',
+    borderColor: '#1ED760',
+    color: '#FFFFFF',
+    transition: 0.5,
+  },
 });
 
 const LinkButton = ({ linkTo, label }) => (
