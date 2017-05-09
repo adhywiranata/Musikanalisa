53eba388a1324e8d36089ff575c6e60792278063
Adhy <adhywiranatap@gmail.com>
2017-05-09 10:25:48 +0700

    refactored AlbumList component to styled components

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,54 +1,94 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const styles = {
-  list: {
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    padding: 100,
-    boxSizing: 'border-box',
-  },
-  card: {
-    width: '20%',
-    margin: '20px 10px',
-    padding: 0,
-    backgroundColor: '#FFFFFF',
-    borderRadius: 5,
-    paddingBottom: 20,
-  },
-};
+import styled from 'styled-components';
+
+const Box = styled.div``;
+
+const RelativeBox = styled.div`
+  position: relative;
+`;
+
+const List = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding: 100px;
+  box-sizing: border-box;
+`;
+
+const Card = styled.div`
+  width: 20%;
+  margin: 20px 10px;
+  padding: 0px;
+  background-color: #FFFFFF;
+  border-radius: 5px;
+  padding-bottom: 20px;
+`;
+
+const ImageOverlay = styled.div`
+  top: 0;
+  width: 100%;
+  height: 98%;
+  position: absolute;
+  background-color: rgba(0,0,0, .5);
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+`;
+
+const Title = styled.h4`
+  color: #FFFFFF;
+  bottom: 20;
+  text-align: center;
+`;
+
+const Text = styled.p`
+  font-size: 0.8em;
+  color: #333333;
+`;
+
+const Ul = styled.ul`
+  padding: 0;
+`;
+
+const Li = styled.li`
+  color: #1ED760;
+  fontSize: 0.8em;
+  padding: 0;
+  list-style-type: none;
+`;
 
 const AlbumList = ({ items }) => (
-  <div style={styles.list}>
+  <List>
     { items.map((item, index) => (
-      <div key={index} style={styles.card}>
-        <div style={{ position: 'relative' }}>
+      <Card key={index}>
+        <RelativeBox>
           <img src={item.images[0].url} width="100%" alt={''} />
-          <div style={{ top: 0, width: '100%', height: '98%', position: 'absolute', backgroundColor: 'rgba(0,0,0, .5)', display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }}>
-            <h4 style={{ color: '#FFFFFF', bottom: 20, textAlign: 'center' }}>{ item.name }</h4>
-          </div>
-        </div>
-        <div>
-          <p style={{ fontSize: '0.8em', color: '#333' }}>Released on: { item.release_date }</p>
-          <p style={{ fontSize: '0.8em', color: '#333' }}>Popularity: { item.popularity }</p>
-          <span style={{ fontSize: '0.8em', color: '#666' }}>Artists</span>
-          <ul style={{ padding: 0 }}>
+          <ImageOverlay>
+            <Title>{ item.name }</Title>
+          </ImageOverlay>
+        </RelativeBox>
+        <Box>
+          <Text>Released on: { item.release_date }</Text>
+          <Text>Popularity: { item.popularity }</Text>
+          <Text>Artists</Text>
+          <Ul style={{ padding: 0 }}>
             { item.artists.map(artist => (
               <Link key={artist.id} to="/" style={{ textDecoration: 'none' }}>
-                <li style={{ color: '#1ED760', fontSize: '0.8em', padding: 0, listStyleType: 'none' }}>
+                <Li>
                   { artist.name }
-                </li>
+                </Li>
               </Link>),
             )}
-          </ul>
-        </div>
-      </div>
+          </Ul>
+        </Box>
+      </Card>
     ),
     )}
-  </div>
+  </List>
 );
 
 export default AlbumList;
