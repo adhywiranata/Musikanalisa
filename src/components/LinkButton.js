b368c49d9e5a5c6e9352920192c0b9e5fb238ba4
Adhy <adhywiranatap@gmail.com>
2017-05-09 10:42:19 +0700

    refactored link buttons to glamorous css

--- src/components/LinkButton.js
+++ src/components/LinkButton.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import glamorous from 'glamorous';
 
-const styles = {
-  btn: {
-    backgroundColor: '#1ED760',
-    padding: 20,
-    border: 0,
-    borderRadius: 10,
-    fontSize: '1.3em',
-    cursor: 'pointer',
-    color: '#FFFFFF',
-    outline: 'none',
-  },
-};
+const Button = glamorous.button({
+  backgroundColor: '#1ED760',
+  padding: 20,
+  border: 0,
+  borderRadius: 10,
+  fontSize: '1.2em',
+  cursor: 'pointer',
+  color: '#FFFFFF',
+  outline: 'none',
+});
 
 const LinkButton = ({ linkTo, label }) => (
   <Link to={linkTo}>
-    <button style={styles.btn}>
-      { label }
-    </button>
+    <Button>{ label }</Button>
   </Link>
 );
 
