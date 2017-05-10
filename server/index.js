8c34d2364127b6ab8cd53bd6c45b866a63f3dcd5
Adhy <adhywiranatap@gmail.com>
2017-05-10 07:13:18 +0700

    added user top read spotify scope

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.get('/login', function(req, res) {
  res.cookie(stateKey, state);
 
  // your application requests authorization
- var scope = 'user-read-private user-read-email user-follow-read user-read-recently-played';
+ var scope = 'user-read-private user-read-email user-follow-read user-read-recently-played user-top-read';
  res.redirect('https://accounts.spotify.com/authorize?' +
    querystring.stringify({
      response_type: 'code',
