6ca6469d66ab4ea89a1b29725d9d626e03f5f83f
Adhy <adhywiranatap@gmail.com>
2017-05-10 07:12:54 +0700

    wired top artists container to router

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import AuthSuccess from './containers/AuthSuccess';
 import MyArtists from './containers/MyArtists';
 import Profile from './containers/Profile';
 import NewReleases from './containers/NewReleases';
+import TopArtists from './containers/TopArtists';
 import { Header } from './components';
 
 class App extends Component {
@@ -20,6 +21,7 @@ class App extends Component {
           <Route path="/login" component={Login} />
           <Route path="/new-releases" component={NewReleases} />
           <Route path="/me" component={Profile} />
+          <Route path="/top" component={TopArtists} />
           <Route path="/callback/:access_token/:refresh_token" component={AuthSuccess} />
         </div>
       </BrowserRouter>
