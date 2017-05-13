ba31c58df13ce8dee101df47027fac05a8d3f8e3
Adhy <adhywiranatap@gmail.com>
2017-05-13 20:14:32 +0700

    refactored App from es6 class to functional

--- src/App.js
+++ src/App.js
@@ -12,25 +12,21 @@ import NewReleases from './containers/NewReleases';
 import TopArtists from './containers/TopArtists';
 import { Header } from './components';
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <Header />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/login" component={Login} />
-            <Route path="/new-releases" component={NewReleases} />
-            <Route path="/me" component={Profile} />
-            <Route path="/artists" component={TopArtists} />
-            <Route path="/artist/:artistId" component={MyArtists} />
-            <Route path="/callback/:access_token/:refresh_token" component={AuthSuccess} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <div className="App">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/new-releases" component={NewReleases} />
+        <Route path="/me" component={Profile} />
+        <Route path="/artists" component={TopArtists} />
+        <Route path="/artist/:artistId" component={MyArtists} />
+        <Route path="/callback/:access_token/:refresh_token" component={AuthSuccess} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
 
 export default App;
