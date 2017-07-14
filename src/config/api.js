288161c1d1c3cf44e5b7c25f00f2379b3823f6d4
Adhy <adhywiranatap@gmail.com>
2017-07-14 23:11:11 +0700

    update api config

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,3 @@
 export const SPOTIFY_CLIENT_ID = '4f38993541a34da0bde6ef4d800e1cad';
 export const SPOTIFY_USER_SCOPES = 'user-read-private user-read-email user-follow-read user-read-recently-played user-top-read';
-export const SPOTIFY_CALLBACK_URL = 'http://localhost:3000/callback';
+export const SPOTIFY_CALLBACK_URL = 'https://musikanalisa.adhywiranata.com/callback';
